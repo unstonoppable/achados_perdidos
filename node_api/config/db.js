15ae4fe4e386632a1f6ffb737f7cddf6cea4bcbd
unknown <josegabrielbeckerdossantos5@gmail.com>
2025-06-14 13:02:26 -0300

    aleluia

--- node_api/config/db.js
+++ node_api/config/db.js
@@ -7,9 +7,10 @@ require('dotenv').config();
 const connectionString = process.env.DATABASE_URL || `postgresql://root:@localhost:5432/achados_e_perdidos`;
 
 const pool = new Pool({
-  connectionString,
-  // A Vercel recomenda esta configuração para conexões de servidor
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_uoWlBSn8GAp5@ep-jolly-silence-a8ccl566-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
